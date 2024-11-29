ae8b60d8ab45bfc2937be92e920738f170ff4fc8
They call me "The drink <costlessnt@gmail.com>
2024-11-28 20:48:45 -0600

    Delete wl.lua

--- wl.lua
+++
@@ -1,9 +0,0 @@
-local plr = game.Players.LocalPlayer
-local whitelist = {
-	105111491,
-	2981703917,
-}
-
-if not (table.find(whitelist, plr.Name) or table.find(whitelist, plr.UserId)) then
-	plr:Kick("mmm... i can feel them wriggling .., <3")
-end
